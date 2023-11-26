429abf5a03978e15c994b0df667386d380ea8a25
s.shtanko <s.shtanko@krista.ru>
2023-11-27 02:58:39 +0300

    can build system for example

--- Alghorithm Silver Polig Hellman/Extension.cs
+++ Alghorithm Silver Polig Hellman/Extension.cs
@@ -9,14 +9,11 @@ namespace Alghorithm_Silver_Polig_Hellman
 {
     public static class Extension
     {
-        public static BigInteger Pow(this BigInteger a, BigInteger b)
+        public static BigInteger BigIntegerModPowWithMinusOne(this BigInteger a, BigInteger b, BigInteger Modulus)
         {
-            BigInteger result = BigInteger.One;
-            for (BigInteger i = 0; i < b; i++)
-            {
-                result = result * a;
-            }
-            return result;
+            if (b == -1)
+                return FindObratniiElement(a, Modulus);
+            return BigInteger.ModPow(a, b, Modulus);
         }
         public static void Print(this List<List<BigInteger>> tableR)
         {
@@ -24,6 +21,33 @@ namespace Alghorithm_Silver_Polig_Hellman
             {
                 Console.WriteLine(String.Join(" ", row));
             }
+        }
+
+
+
+
+        private static BigInteger FindObratniiElement(BigInteger a, BigInteger m)
+        {
+            BigInteger x, y;
+            BigInteger g = GCD(a, m, out x, out y);
+            if (g != 1)
+                throw new ArgumentException();
+            return (x % m + m) % m;
+        }
+
+        private static BigInteger GCD(BigInteger a, BigInteger b, out BigInteger x, out BigInteger y)
+        {
+            if (a == 0)
+            {
+                x = 0;
+                y = 1;
+                return b;
+            }
+            BigInteger x1, y1;
+            BigInteger d = GCD(b % a, a, out x1, out y1);
+            x = y1 - (b / a) * x1;
+            y = x1;
+            return d;
         }
     }
 }
--- Alghorithm Silver Polig Hellman/Program.cs
+++ Alghorithm Silver Polig Hellman/Program.cs
@@ -7,6 +7,7 @@ class SilverPohligHellman
 {
     static void Main()
     {
+        Console.WriteLine(BigInteger.ModPow(28, 18, 37));
         //https://cyberleninka.ru/article/n/primer-vychisleniya-diskretnogo-logarifma/viewer
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         Console.WriteLine("Алгоритм Сильвера-Полига-Хеллмана");
@@ -38,27 +39,58 @@ class SilverPohligHellman
             .ToList();
         Console.WriteLine(String.Join(", ", q1q2qn));
         List<List<BigInteger>> tableR = Step1_BuildTableOfR(a, p, q1q2qn);
+        Console.WriteLine("R Table:");
         tableR.Print();
-
+        Console.WriteLine("System:");
+        var system = Step2(q1q2qn, tableR, a, b, p);
+        foreach (var item in system)
+        {
+            Console.WriteLine($"x ≡ {item.b} (mod {item.p})");
+        }
 
 
         return BigInteger.MinusOne;
     }
 
-    static BigInteger Step2()
+    static List<(BigInteger b, BigInteger p)> Step2(
+        List<(BigInteger prime, BigInteger pow)> q1q2qn, 
+        List<List<BigInteger>> tableR, BigInteger a, BigInteger b, BigInteger p)
     {
         //tex:
         //$$x\equiv\log_a{b}\equiv x_0+x_1q_i+...+x_{\alpha_{i}-1}q_i^{\alpha_{i}-1}\pmod{q_i^{\alpha_i}},$$
-        
-
-
-
-        throw new NotImplementedException();
+        //$$a^{x_{j}\cdot\frac{p-1}{q_i}} \equiv (ba^{-x_0-x_1q_i...-x_{j-1}q_i^{j-1}})^{\frac{p-1}{q_i^{j+1}}} \pmod{p}$$
+        int k = tableR.Count;
+        List<(BigInteger b, BigInteger p)> result = new();
+        for (int i = 0; i < k; i++)
+        {
+            var currentRow = tableR[i];
+            var qi = q1q2qn[i].prime;
+            var qi_stepen = q1q2qn[i].pow;
+            BigInteger xi = 0;
+            BigInteger skobochki = b;
+            for (int j = 0; j < qi_stepen; j++)
+            {
+                BigInteger stepen = (p - 1) / BigInteger.Pow(qi, j+1);
+                BigInteger currentX = Step2_GetXi(currentRow, skobochki, stepen, p);
+                if (j != qi_stepen - 1)
+                    skobochki = skobochki * Extension.BigIntegerModPowWithMinusOne(a, -currentX, p);
+                //??? Нужно ли каждый раз -currentX или только в первый раз (потом без -)
+                xi += currentX * BigInteger.ModPow(qi, j, p);
+            }
+            result.Add((xi, BigInteger.ModPow(q1q2qn[i].prime, q1q2qn[i].pow, 99999999)));//??? Магическое число...
+        }
+        return result;
     }
-    static void Step2_Formula()
+    static BigInteger Step2_GetXi(List<BigInteger> tableRRow, 
+        BigInteger skobochki, BigInteger stepen, BigInteger p)
     {
-        //tex:
-        //$$a^{x\cdot\frac{p-1}{q_i}} \equiv b^{\frac{p-1}{q_i}} \pmod{p}$$
+        var temp = BigInteger.ModPow(skobochki, stepen, p);
+        for (int i = 0; i < tableRRow.Count;i++)
+        {
+            if (tableRRow[i] == temp)
+                return i;
+        }
+        throw new ArgumentException($"Не найдено решение в R таблице!");
     }
 
     static List<List<BigInteger>> Step1_BuildTableOfR(BigInteger a, BigInteger p, List<(BigInteger prime, BigInteger pow)> q1q2qn)
